22589bfb72c06f49a8ee119000e51315b69a7d7f
Yue Amir Zhou <amirzhou001@gmail.com>
2025-03-11 18:13:20 -0600

    chore(setup): config typeorm and migration target, add one test entity

--- apps/clarte-api/src/app.module.ts
+++ apps/clarte-api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 7777,
+      username: 'bitravage',
+      password: '693721',
+      database: 'clarte',
+      entities: [],
+      synchronize: true, // do not use this in production
+      retryAttempts: 10,
+      retryDelay: 5000,
+      autoLoadEntities: false,
+      migrations: ['src/database/migrations/*.ts'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
