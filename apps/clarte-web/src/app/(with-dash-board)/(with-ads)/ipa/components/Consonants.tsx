ed350dcb4c16aca14cd781dd84473755e7ce794b
Yue Amir Zhou <amirzhou001@gmail.com>
2025-04-09 16:29:13 -0600

    Critic: change tsconfig for web app

--- apps/clarte-web/src/app/(with-dash-board)/(with-ads)/ipa/components/Consonants.tsx
+++ apps/clarte-web/src/app/(with-dash-board)/(with-ads)/ipa/components/Consonants.tsx
@@ -2,52 +2,20 @@
 import { ipaConsonants } from '@/utils';
 import IPAIcon from './IPAIcon';
 import { useIPA } from '../context/IPAContext';
-import IPADetails from './IPADetails';
 
 export default function Consonants() {
   const { activeIPA, setActiveIPA } = useIPA();
   return (
     <>
-      <div className="flex flex-col gap-6">
-        <h2 className="font-bold text-4xl text-emerald-600">Consonants</h2>
-
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <h3 className="font-semibold text-2xl">Voiced</h3>
-            <div className="flex gap-4 flex-wrap">
-              {ipaConsonants.voiced.map((voiced) => (
-                <IPAIcon
-                  key={voiced.name}
-                  ipa={voiced}
-                  onSelect={setActiveIPA}
-                  isActive={activeIPA === voiced}
-                />
-              ))}
-            </div>
-            <IPADetails
-              activeIPA={activeIPA}
-              categoryIPAs={ipaConsonants.voiced}
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h3 className="font-semibold text-2xl">Voiceless</h3>
-            <div className="flex gap-4 flex-wrap">
-              {ipaConsonants.voiceless.map((voiceless) => (
-                <IPAIcon
-                  key={voiceless.name}
-                  ipa={voiceless}
-                  onSelect={setActiveIPA}
-                  isActive={activeIPA === voiceless}
-                />
-              ))}
-            </div>
-            <IPADetails
-              activeIPA={activeIPA}
-              categoryIPAs={ipaConsonants.voiceless}
-            />
-          </div>
-        </div>
+      <div className="flex gap-4 flex-wrap">
+        {[...ipaConsonants.voiced, ...ipaConsonants.voiceless].map((c) => (
+          <IPAIcon
+            key={c.name}
+            ipa={c}
+            onSelect={setActiveIPA}
+            isActive={activeIPA === c}
+          />
+        ))}
       </div>
     </>
   );
--- apps/clarte-web/src/app/(with-dash-board)/(with-ads)/ipa/page.tsx
+++ apps/clarte-web/src/app/(with-dash-board)/(with-ads)/ipa/page.tsx
@@ -1,17 +1,18 @@
 // route:/ipa
 import { Consonants, Vowels } from './components';
 import { IPAProvider } from './context/IPAContext';
+import IPAInfo from './components/IPAInfo';
 
 export default function Page() {
   return (
     <IPAProvider>
-      <div className="w-full min-w-lg flex flex-col gap-16">
-        <div>
+      <div className="border flex gap-16 w-full justify-center">
+        <div className=" w-80 flex flex-col gap-4">
+          <h2>French IPA Symbols</h2>
           <Vowels />
-        </div>
-        <div>
           <Consonants />
         </div>
+        <IPAInfo />
       </div>
     </IPAProvider>
   );
