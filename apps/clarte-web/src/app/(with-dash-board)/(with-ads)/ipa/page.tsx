c003897f24b2538b28f36c182ea3aeda1ed6ebb9
Yue Amir Zhou <amirzhou001@gmail.com>
2025-04-26 11:27:10 -0600

    feat: fix envs

--- apps/clarte-web/src/app/(with-dash-board)/(with-ads)/ipa/page.tsx
+++ apps/clarte-web/src/app/(with-dash-board)/(with-ads)/ipa/page.tsx
@@ -4,8 +4,8 @@ import { IPASymbolIcon } from '@/components/icons';
 import IpaInteractionWrapper from './components/IpaInteractionWrapper';
 
 async function getIpaData(): Promise<IpaWithExamplesDto[]> {
-  const internalApiUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/proxy/ipa-symbols/all` // Use Route Handler
+  const internalApiUrl = process.env.NEXT_PUBLIC_SITE_URL
+    ? `https://${process.env.NEXT_PUBLIC_SITE_URLL}/api/proxy/ipa-symbols/all` // Use Route Handler
     : 'http://localhost:3000/api/proxy/ipa-symbols/all'; // Fallback for local dev
 
   try {
